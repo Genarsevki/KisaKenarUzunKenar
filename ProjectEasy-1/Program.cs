792f36544dce41b99e6c55890a391c7192b004d6
Genarsevki <genar.sevkis@gmail.com>
2024-07-20 13:47:33 +0300

    Proje dosyası ekle.

---
+++ ProjectEasy-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEasy_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kısa Kenar :");
+            int kisa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Uzun Kenar : ");
+            int uzun = int.Parse(Console.ReadLine());
+
+            int alan = kisa * uzun;
+            int cevre = (kisa + uzun) * 2;
+
+            Console.WriteLine($"alan: {alan} Çevre: {cevre}");
+        }
+    }
+}
